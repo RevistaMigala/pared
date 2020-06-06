2aab58c389d91bab511c46d40d961c30b7edd0a9
David Enriquez <davidenriq11@gmail.com>
2020-06-06 13:45:04 -0500

    Add local mongo script

---
+++ local-db/start-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MONGODB_FOLDER=
+export MONGODB_DATA_FOLDER=
+
+echo $MONGODB_FOLDER
+echo $MONGODB_FOLDER
+echo "Starting MongoDB database"
+
+sudo /Users/$USER/$MONGODB_FOLDER/bin/mongod --dbpath=/Users/$USER/$MONGODB_DATA_FOLDER
