ef686617519c44bfefefff32036f894b0b0ac174
David Enriquez <davidenriq11@gmail.com>
2020-06-06 13:44:37 -0500

    Add mongodb

---
+++ db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const connectionURL = process.env.CONNECTION_URL
+
+mongoose.connect(connectionURL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
