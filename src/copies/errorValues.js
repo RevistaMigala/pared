6ec0943d9e89d3390d9575183402caf22c24c3fd
David Enriquez <davidenriq11@gmail.com>
2020-06-23 23:06:32 -0500

    Index copies

---
+++ src/copies/errorValues.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: '404 not found',
+    description: 'The requested page does not exists',
+    link: 'https://migala.mx',
+    GATag: process.env.GA_TAG,
+    name: 'Created by Migala.mx',
+    submit: 'Submit',
+    about: 'About',
+    langDefault: 'en',
+    langOptionA: 'es',
+    exercise1: 'Identity/Lie',
+}
---
+++ src/copies/index.js
@@ -0,0 +1,17 @@
+const indexValues = require('./indexPage')
+const aboutValues = require('./aboutPage')
+const submitValues = require('./submitPage')
+const submitValuesSuccess = require('./submitPageSuccess')
+const submitValuesError = require('./submitPageError')
+const exercise1Values = require('./exercise1Page')
+const errorValues = require('./errorValues')
+
+module.exports = {
+    indexValues,
+    aboutValues,
+    submitValues,
+    submitValuesSuccess,
+    submitValuesError,
+    exercise1Values,
+    errorValues,
+}
