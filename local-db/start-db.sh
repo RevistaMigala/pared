eb470291020af037bb7ade837953233bc3f87105
David Enríquez <davidenriquez@MacBook-Pro-de-David.local>
2020-07-01 01:15:34 -0500

    Update start example

--- local-db/start-db.sh
+++ local-db/start-db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export MONGODB_FOLDER=mongobongo
-export MONGODB_DATA_FOLDER=mongo-data
+export MONGODB_FOLDER=dbb/mongodb-local
+export MONGODB_DATA_FOLDER=dbb/mongodb-local-data
 
 echo $MONGODB_FOLDER
-echo $MONGODB_FOLDER
+echo $MONGODB_DATA_FOLDER
 echo "Starting MongoDB database"
 
 sudo /Users/$USER/$MONGODB_FOLDER/bin/mongod --dbpath=/Users/$USER/$MONGODB_DATA_FOLDER
